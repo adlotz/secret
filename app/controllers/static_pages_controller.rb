24b4701a748fa1b38663a37d5211dae9b7ca5a4f
adlotz <a.lotz@hotmail.fr>
2018-02-06 11:17:26 +0100

    heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,11 @@ class StaticPagesController < ApplicationController
   end
 
   def secret
+  	
+  	unless logged_in?
+  		flash[:danger] = 'You must be connected to know about the secret !'
+  		redirect_to login_path
+	end
+
   end
 end
