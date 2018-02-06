13e815e9e0d82d31907f11ed95922a979bf6c0cd
adlotz <a.lotz@hotmail.fr>
2018-02-06 11:13:32 +0100

    heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,5 @@ class StaticPagesController < ApplicationController
   end
 
   def secret
-  	unless logged_in?
-  		flash.now[:danger] = "hey login-toi pour y accéder, c'est bien plus poli !"
-  		redirect_to login_path
   end
 end
